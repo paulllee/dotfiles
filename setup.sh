be84e076d6727a2852a23d51e4e7e8dca11c1e70
Paul <paulllee.md@gmail.com>
2024-01-23 15:22:01 -0500

    simplify setup with dotsync

--- setup.sh
+++ setup.sh
@@ -28,17 +28,7 @@ fi
 
 git clone --depth=1 https://github.com/paulllee/dotfiles.git ~/.dotfiles
 
-bash ~/.dotfiles/dotfiles/.local/bin/dot-filesync
-bash ~/.dotfiles/dotfiles/.local/bin/dot-gitsync
-bash ~/.dotfiles/dotfiles/.local/bin/dot-setdefaults
-
-printf_h "Font Installation"
-
-read -p "This will install 'MesloLDS Nerd Font' in your Font Book. Continue? [y/n]: " -r
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    rsync -aP ~/.dotfiles/fonts/ ~/Library/Fonts/
-fi
+/bin/bash ~/.dotfiles/dotfiles/.local/bin/dotsync -bdfg
 
 printf_h "SSH Key Generation"
 
@@ -46,14 +36,6 @@ read -p "This will generate a new key to '~/.ssh/id_rsa'. Continue? [y/n]: " -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     yes "y" | ssh-keygen -t ed25519 -N "" -f ~/.ssh/id_rsa
-fi
-
-printf_h "Package Installation"
-
-read -p "This will install all applications in the Brewfile. Continue? [y/n]: " -r
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    brew bundle --file ~/.dotfiles/Brewfile --no-lock
 fi
 
 printf_h "Setup Complete! Next Steps"
