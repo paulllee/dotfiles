b53e8e7211a6420e95854fcc5fff870b9369b700
Paul Lee <paulllee.md@gmail.com>
2024-01-23 19:55:35 -0500

    update dotsync flags

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,12 +14,12 @@ This will rename ~/.dotfiles to ~/.dotfiles-old to prevent conflicts if it alrea
 
 [[ -d ~/.dotfiles ]] && rm -rf ~/.dotfiles-old && mv ~/.dotfiles ~/.dotfiles-old
 git clone --depth=1 https://github.com/paulllee/dotfiles.git ~/.dotfiles && \
-    /bin/bash ~/.dotfiles/dotfiles/.local/bin/dotsync -bdfg
+    /bin/bash ~/.dotfiles/dotfiles/.local/bin/dotsync -bgms
 
 printf "Setup completed! Next steps:
     - Change the font of Apple Terminal to 'MesloLGS NF'.
     - Create a new SSH key via 'auto-keygen' and add it to services (e.g. GitHub).
-    - TIP: You can sync your dotfiles with new remote changes in the future with 'dsf'.
+    - TIP: You can sync your dotfiles with new remote changes in the future with 'dotsync -s'.
       - View 'dotsync -h' for help.
 \n"
 
