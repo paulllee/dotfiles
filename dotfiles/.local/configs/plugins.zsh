ff8124c5b0a16b268457849dfda25c297e2a346c
Paul Lee <paulllee.md@gmail.com>
2024-01-30 18:50:04 -0500

    re-add syntax highlighting

--- dotfiles/.local/configs/plugins.zsh
+++ dotfiles/.local/configs/plugins.zsh
@@ -4,8 +4,7 @@ source "$(brew --prefix)/opt/fzf/shell/completion.zsh"
 # https://github.com/zsh-users/zsh-autosuggestions
 source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 
-# must be sourced LAST
-# https://github.com/zsh-users/zsh-syntax-highlighting
+# https://github.com/zsh-users/zsh-syntax-highlighting - must be sourced LAST
 source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # https://github.com/sindresorhus/pure
