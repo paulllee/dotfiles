1077cddca3db4c8a0d1f0c4cba52a77591f93556
Paul Lee <paulllee.md@gmail.com>
2024-01-30 23:34:39 -0500

    add double quotes

--- dotfiles/.local/command/plugins.zsh
+++ dotfiles/.local/command/plugins.zsh
@@ -2,10 +2,10 @@
 source "$(brew --prefix)/opt/fzf/shell/completion.zsh"
 
 # https://github.com/zsh-users/zsh-autosuggestions
-source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source "$(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
 
 # https://github.com/zsh-users/zsh-syntax-highlighting - must be sourced LAST
-source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source "$(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 
 # https://github.com/sindresorhus/pure
 autoload -U promptinit; promptinit
