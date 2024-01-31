65ad33a7b3d64f0c64d6aee53a4129d71cefde97
Paul Lee <paulllee.md@gmail.com>
2024-01-31 15:21:18 -0500

    modify ls to ignore DS_Store

--- dotfiles/.zsh/aliases.zsh
+++ dotfiles/.zsh/aliases.zsh
@@ -3,8 +3,8 @@
 alias auto-keygen='yes "y" | ssh-keygen -t ed25519 -N "" -f ~/.ssh/id_rsa'
 
 # ls and lsg(ithub) - uses eza for a modern replacement for ls
-alias ls='eza -la'
-alias lsg='eza -la --git --git-repos'
+alias ls='eza -la --ignore-glob ".DS_Store"'
+alias lsg='ls --git --git-repos'
 
 # q(uick) cd - uses fuzzy search from home to change directories
 alias qcd='cd $(fd . ~ --type d -E ".git" | fzf)'
