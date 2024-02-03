d3b4f107da1f8658c56c6c379d330069f224ef06
Paul Lee <paulllee.md@gmail.com>
2024-02-03 15:56:35 -0500

    ensure config dir is in home for mac

--- dotfiles/.zsh/pre/variables.zsh
+++ dotfiles/.zsh/pre/variables.zsh
@@ -1,3 +1,6 @@
+# ensure config dir is in default $HOME/.config
+export XDG_CONFIG_HOME="$HOME/.config"
+
 # disables Homebrew's anonymous analytics - https://docs.brew.sh/Analytics
 export HOMEBREW_NO_ANALYTICS=1
 
