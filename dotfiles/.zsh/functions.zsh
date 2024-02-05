1b62ea1e08e714ad1aaec1db1d0cdf9c1bc45413
Paul Lee <paulllee.md@gmail.com>
2024-02-05 15:35:48 -0500

    add proper indentation and remove excessive comments

--- dotfiles/.zsh/functions.zsh
+++ dotfiles/.zsh/functions.zsh
@@ -1,20 +1,17 @@
 IGNORE=".DS_Store|.git"
 
-# auto-(ssh-)keygen - non-interactive ssh-keygen with best practice (defaults to $HOME/.ssh/id_rsa)
-function auto-keygen() {
-  KEYGEN_PATH="${1:-$HOME/.ssh/id_rsa}"
-  ssh-keygen -t ed25519 -N "" -f $KEYGEN_PATH
+function auto-keygen() {  # auto-(ssh-)keygen
+    KEYGEN_PATH="${1:-$HOME/.ssh/id_rsa}"
+    ssh-keygen -t ed25519 -N "" -f $KEYGEN_PATH
 }
 
-# ls - uses eza for a modern replacement for ls
-function ls() {
-  eza -la --ignore-glob=$IGNORE --icons=auto --git --git-repos
+function ls() {  # (modern-)ls
+    eza -la --ignore-glob=$IGNORE --icons=auto --git --git-repos
 }
 
-# q(uick) cd - uses fuzzy search to change directories from any directory (defaults to $HOME)
-function qcd() {
-  BASE="${1:-$HOME}"
-  cd $(fd --hidden --type d . $BASE | fzf --ansi --border --preview "tree -a -I '$IGNORE' -C {}")
-  clear
+function qcd() {  # q(uick)-cd
+    BASE="${1:-$HOME}"
+    cd $(fd --hidden --type d . $BASE | fzf --ansi --border --preview "tree -a -I '$IGNORE' -C {}")
+    clear
 }
 
