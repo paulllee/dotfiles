2c08a579170733c9523ae122fd58a412e779fe5c
Paul Lee <paulllee.md@gmail.com>
2024-02-05 16:21:27 -0500

    add comments

--- dotfiles/.zsh/pre/paths.zsh
+++ dotfiles/.zsh/pre/paths.zsh
@@ -1,8 +1,6 @@
+# dotsync binary
 path+=("$HOME/.local/bin")
 
-# this was added for fzf
-path+=("$(brew --prefix)/opt/fzf/bin")
-
-# this was added for Pure
 # https://github.com/sindresorhus/pure?tab=readme-ov-file#homebrew
 fpath+=("$(brew --prefix)/share/zsh/site-functions")
+
