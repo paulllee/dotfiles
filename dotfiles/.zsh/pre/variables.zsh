85446cc450d3407574ddbd0e45937c6aa9d3f8cc
Paul Lee <paulllee.md@gmail.com>
2024-02-05 15:59:05 -0500

    fix mocha for fzf

--- dotfiles/.zsh/pre/variables.zsh
+++ dotfiles/.zsh/pre/variables.zsh
@@ -4,13 +4,13 @@ export XDG_CONFIG_HOME="$HOME/.config"
 # https://docs.brew.sh/Analytics
 export HOMEBREW_NO_ANALYTICS=1
 
-# default to nvim
+# setting my default editor to nvim for now
 export EDITOR=nvim
 export VISUAL="$EDITOR"
 
-# catppuccin-mocha theme for fzf
-export FZF_DEFAULT_OPTS="--color=\
-    bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8,\
-    fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc,\
-    marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+# mocha variant - https://github.com/catppuccin/fzf?tab=readme-ov-file#usage
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
