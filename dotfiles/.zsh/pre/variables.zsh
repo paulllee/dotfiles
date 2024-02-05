4396a110f00a08bc47d292f606429483bb19d2bd
Paul Lee <paulllee.md@gmail.com>
2024-02-05 15:44:08 -0500

    add condensed comments

--- dotfiles/.zsh/pre/variables.zsh
+++ dotfiles/.zsh/pre/variables.zsh
@@ -1,9 +1,16 @@
-# ensure config dir is in default $HOME/.config
+# ensure ~/.config is default across all applications
 export XDG_CONFIG_HOME="$HOME/.config"
 
-# disables Homebrew's anonymous analytics - https://docs.brew.sh/Analytics
+# https://docs.brew.sh/Analytics
 export HOMEBREW_NO_ANALYTICS=1
 
-# change editor to helix
-export EDITOR=hx
+# default to nvim
+export EDITOR=nvim
 export VISUAL="$EDITOR"
+
+# catppuccin-mocha theme for fzf
+export FZF_DEFAULT_OPTS="--color=\
+    bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8,\
+    fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc,\
+    marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+
