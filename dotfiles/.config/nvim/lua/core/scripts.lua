b281bb61b062ec3adfa3f614e414cf09be8e722e
Paul Lee <paulllee.md@gmail.com>
2024-02-08 23:58:02 -0500

    remove auto format script

--- dotfiles/.config/nvim/lua/core/scripts.lua
+++
@@ -1,7 +0,0 @@
--- format on `:w`
-vim.api.nvim_create_autocmd('BufWritePre', {
-    buffer = vim.fn.bufnr(),
-    callback = function()
-        vim.lsp.buf.format({ timeout_ms = 3000 })
-    end,
-})
