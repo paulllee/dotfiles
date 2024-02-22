09ddea3e9f01d24ba499f488663838939cdfd1bf
Paul Lee <paulllee.md@gmail.com>
2024-02-21 20:51:36 -0500

    add treesitter

--- dotfiles/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/plugins/treesitter.lua
@@ -4,21 +4,43 @@ return {
     build = ":TSUpdate",
     opts = {
       ensure_installed = {
+        -- config formats
+        "json",
+        "jsonc",
+        "requirements",
+        "xml",
+        "toml",
+        "yaml",
+
+        -- docs
+        "luadoc",
+        "markdown",
+        "markdown_inline",
+        "vimdoc",
+
+        -- other
+        "csv",
+        "gitignore",
+        "regex",
+
+        -- programming languages
+        "python",
         "bash",
         "c",
         "cpp",
+        "lua",
+        "ocaml",
+        "ocaml_interface",
+        "rust",
+        "sql",
+
+        -- web dev
         "css",
         "html",
+        "htmldjango",
         "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "regex",
-        "rust",
-        "toml",
-        "yaml"
+        "tsx",
+        "typescript"
       },
       highlight = { enable = true },
       indent = { enable = true }
