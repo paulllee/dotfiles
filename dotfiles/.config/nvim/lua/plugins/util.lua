b0c423998bac792c15dc1a92b11c9c9d056b7658
Paul Lee <paulllee.md@gmail.com>
2024-02-24 18:56:44 -0500

    move telescope to utils

--- dotfiles/.config/nvim/lua/plugins/util.lua
+++ dotfiles/.config/nvim/lua/plugins/util.lua
@@ -1,5 +1,22 @@
 return {
   {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        pickers = {
+          find_files = {
+            find_command = { "fd", "--hidden", "--type", "f" }
+          },
+          live_grep = {
+            additional_args = { "--hidden", "--glob=!.git" }
+          }
+        }
+      })
+    end
+  },
+  {
     "echasnovski/mini.comment",
     config = function()
       require("mini.comment").setup()
