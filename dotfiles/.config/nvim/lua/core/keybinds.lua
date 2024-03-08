079777d1642a46ab7c2a2c86741fe58480851e51
Paul Lee <paulllee.md@gmail.com>
2024-03-08 17:40:35 -0500

    simplify keybinds and add LspRestart

--- dotfiles/.config/nvim/lua/core/keybinds.lua
+++ dotfiles/.config/nvim/lua/core/keybinds.lua
@@ -7,14 +7,20 @@ local builtin = require("telescope.builtin")
 local mappings = {}
 local opts = { prefix = "<space>" }
 
+mappings.l = {
+  name = "[L]sp",
+  i = { "<cmd>:LspInfo<cr>", "[I]nfo" },
+  r = { "<cmd>:LspRestart<cr>", "[_R]estart" }
+}
+
 mappings.n = {
   name = "[N]eo-tree",
   e = { "<cmd>:Neotree<cr>", "[E]xplorer" },
   b = { "<cmd>:Neotree buffers<cr>", "[B]uffer" }
 }
 
-mappings.t = {
-  name = "[T]elescope",
+mappings.g = {
+  name = "[G]o To",
   f = { builtin.find_files, "[F]iles" },
   g = { builtin.live_grep, "[G]rep" },
   b = { builtin.buffers, "[B]uffer" }
@@ -25,33 +31,18 @@ wk.register(mappings, opts)
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(args)
     opts.buffer = args.buf
-    mappings.c = {
-      name = "[C]ode",
-      a = { vim.lsp.buf.code_action, "[A]ction" }
-    }
-    mappings.d = {
-      name = "[D]iagnostic",
-      o = { vim.diagnostic.open_float, "[O]pen" },
-      p = { vim.diagnostic.goto_prev, "[P]revious" },
-      n = { vim.diagnostic.goto_next, "[N]ext" },
-      e = { vim.diagnostic.open_float, "[F]loat Message" },
-      q = { vim.diagnostic.setloclist, "[Q]uick Fix List" }
-    }
-    mappings.f = { vim.lsp.buf.format, "[F]ormat" }
     mappings.g = {
-      name = "[G]o To",
-      D = { vim.lsp.buf.declaration, "[^D]eclaration" },
-      d = { vim.lsp.buf.definition, "[_D]efinition" },
-      i = { vim.lsp.buf.implementation, "[I]mplementation" },
-      t = { vim.lsp.buf.type_definition, "[T]ype Definition" },
+      d = { builtin.lsp_definitions, "[_D]efinition" },
+      D = { builtin.diagnostics, "[^D]iagnostics" },
       h = { vim.lsp.buf.hover, "[H]over" },
-      s = { vim.lsp.buf.signature_help, "[S]ignature Help" }
+      i = { builtin.lsp_implementations, "[I]mplementation" },
+      r = { builtin.lsp_references, "[R]eferences" },
+      s = { vim.lsp.buf.signature_help, "[S]ignature Help" },
+      t = { builtin.lsp_type_definitions, "[T]ype Definition" }
     }
-    mappings.r = { vim.lsp.buf.rename, "[R]ename All References" }
-    mappings.t = {
-      name = "[T]elescope With LSP",
-      d = { builtin.diagnostics, "[D]iagnostics" },
-      r = { builtin.lsp_references, "[R]eferences" }
+    mappings.l = {
+      f = { vim.lsp.buf.format, "[F]ormat" },
+      R = { vim.lsp.buf.rename, "[^R]ename All References" }
     }
     wk.register(mappings, opts)
   end
