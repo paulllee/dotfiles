406434934802ceace9a5b0dc9caa5b2b0d726726
Paul Lee <paulllee.md@gmail.com>
2024-03-11 11:25:04 -0400

    clean up new keybinds

--- dotfiles/.config/nvim/lua/core/keybinds.lua
+++ dotfiles/.config/nvim/lua/core/keybinds.lua
@@ -1,3 +1,8 @@
+-- aliases
+local ts = require("telescope.builtin")
+local ls = require("luasnip")
+local lb = vim.lsp.buf
+
 local function map(mode, lhs, rhs, desc, buffer)
   vim.keymap.set(mode, lhs, rhs, {
     desc = desc,
@@ -5,38 +10,32 @@ local function map(mode, lhs, rhs, desc, buffer)
   })
 end
 
-local builtin = require("telescope.builtin")
-
 map("n", "<Space>n", "<cmd>:Neotree<cr>", "Neotree")
-map("n", "<Space>r", "<cmd>:LspRestart<cr>", "Restart server")
+map("n", "<Space>r", "<cmd>:LspRestart<cr>", "Restart lsp")
 
-map("n", "<Space>b", builtin.buffers, "Telescope buffers")
-map("n", "<Space>f", builtin.find_files, "Telescope find files")
-map("n", "<Space>g", builtin.live_grep, "Telescope live grep")
-map("n", "<Space>o", builtin.oldfiles, "Telescope old files")
+map("n", "<Space>b", ts.buffers, "Telescope buffers")
+map("n", "<Space>f", ts.find_files, "Telescope find files")
+map("n", "<Space>g", ts.live_grep, "Telescope live grep")
+map("n", "<Space>o", ts.oldfiles, "Telescope old files")
 
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(args)
-    local function lsp_map(mode, lhs, rhs, desc)
+    local function lmap(mode, lhs, rhs, desc)
       map(mode, lhs, rhs, desc, args.buf)
     end
 
-    local lsp_buf = vim.lsp.buf
+    lmap("n", "gd", ts.lsp_definitions, "Definitions")
+    lmap("n", "gD", ts.diagnostics, "Diagnostics")
+    lmap("n", "gi", ts.lsp_implementations, "Implementations")
+    lmap("n", "gr", ts.lsp_references, "References")
 
-    lsp_map("n", "gd", builtin.lsp_definitions, "Definitions")
-    lsp_map("n", "gD", builtin.diagnostics, "Diagnostics")
-    lsp_map("n", "gi", builtin.lsp_implementations, "Implementations")
-    lsp_map("n", "gr", builtin.lsp_references, "References")
+    lmap("n", "gh", lb.hover, "Hover")
+    lmap("n", "gH", lb.signature_help, "Signature help")
 
-    lsp_map("n", "gh", lsp_buf.hover, "Hover")
-    lsp_map("n", "gH", lsp_buf.signature_help, "Signature help")
-
-    lsp_map("n", "<Space>F", lsp_buf.format, "Format file")
-    lsp_map("n", "<Space>R", lsp_buf.rename, "Rename all references")
+    lmap("n", "<Space>F", lb.format, "Format file")
+    lmap("n", "<Space>R", lb.rename, "Rename all references")
   end
 })
 
-local ls = require("luasnip")
-
-map({ "i", "s" }, "<C-j>", function() ls.jump(-1) end, "LS Jump left")
-map({ "i", "s" }, "<C-k>", function() ls.jump(1) end, "LS Jump right")
+map({ "i", "s" }, "<C-j>", function() ls.jump(-1) end, "LS jump left")
+map({ "i", "s" }, "<C-k>", function() ls.jump(1) end, "LS jump right")
