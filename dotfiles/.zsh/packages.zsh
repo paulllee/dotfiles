e4c7b55383272d516b4b65fdb19cd37cbb2df035
Paul Lee <paulllee.md@gmail.com>
2024-03-19 23:46:51 -0400

    switch to starship

--- dotfiles/.zsh/packages.zsh
+++ dotfiles/.zsh/packages.zsh
@@ -1,7 +1,5 @@
 source "$(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
 source "$(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 
-autoload -U promptinit; promptinit
-prompt pure
-
 eval "$(micromamba shell hook --shell zsh)"
+eval "$(starship init zsh)"
--- dotfiles/.zsh/paths.zsh
+++ dotfiles/.zsh/paths.zsh
@@ -1,2 +1,1 @@
 path+=("$HOME/.local/bin")
-fpath+=("$(brew --prefix)/share/zsh/site-functions")
