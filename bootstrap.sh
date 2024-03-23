424cd6c9aeb720d52e916d0b1aee205ec3c7c9a0
Paul Lee <paulllee.md@gmail.com>
2024-03-23 09:10:37 -0400

    adding fish installation in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+set -e
+
 /usr/bin/env bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && \
-  eval "$(/opt/homebrew/bin/brew shellenv)" || \
-  exit 1  # exit IF Homebrew installation returns a non zero code
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 
 rm -rf "$HOME/.dotfiles" && \
   git clone --depth=1 https://github.com/paulllee/dotfiles.git "$HOME/.dotfiles" && \
   /usr/bin/env bash "$HOME/.dotfiles/dotfiles/.local/bin/ds" -dgmp
+
+printf "Changing default shell to fish will prompt for sudo access.\n"
+echo "/opt/homebrew/bin/fish" | sudo tee -a "/etc/shells" && \
+  chsh -s "/opt/homebrew/bin/fish"
