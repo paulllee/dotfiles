99c775b70238c2ee64600550e2c75873bab20ed3
Paul Lee <paulllee.md@gmail.com>
2024-03-30 14:12:41 -0400

    add 4 spaces

--- dotfiles/.config/nvim/ftplugin/ps1.lua
+++ dotfiles/.config/nvim/ftplugin/ps1.lua
@@ -1,4 +1,4 @@
--- https://www.cs.umd.edu/~nelson/classes/resources/cstyleguide
+-- https://poshcode.gitbook.io/powershell-practice-and-style/style-guide
 vim.bo.expandtab = true
 vim.bo.shiftwidth = 4
 vim.bo.softtabstop = 4
