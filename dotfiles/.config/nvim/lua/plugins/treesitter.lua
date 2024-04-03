d00cf2534433a778704ca2b3e3e0c624249c7251
Paul Lee <paulllee.md@gmail.com>
2024-04-03 14:26:42 -0400

    prefer auto install instead of listing them all

--- dotfiles/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/plugins/treesitter.lua
@@ -4,34 +4,7 @@ return {
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "bash",
-          "c",
-          "cpp",
-          "css",
-          "csv",
-          "gitignore",
-          "html",
-          "javascript",
-          "json",
-          "jsonc",
-          "lua",
-          "markdown",
-          "markdown_inline",
-          "python",
-          "query",
-          "regex",
-          "requirements",
-          "rust",
-          "sql",
-          "toml",
-          "typescript",
-          "tsx",
-          "vim",
-          "vimdoc",
-          "xml",
-          "yaml"
-        },
+        auto_install = true,
         highlight = {
           enable = true,
           additional_vim_regex_highlighting = true
