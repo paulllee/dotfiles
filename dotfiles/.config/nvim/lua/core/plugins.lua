c52b4f9182f147438a4eb2b13c65efa02c099778
Paul Lee <paulllee.md@gmail.com>
2024-05-23 00:56:52 -0400

    update plugins with new helpers and specs support

--- dotfiles/.config/nvim/lua/core/plugins.lua
+++ dotfiles/.config/nvim/lua/core/plugins.lua
@@ -6,16 +6,18 @@ if not vim.loop.fs_stat(lazypath) then
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable",
-    lazypath,
+    lazypath
   }
 end
 vim.opt.rtp:prepend(lazypath)
 
+local helpers = require("support.helpers")
+local specs = require("support.specs")
 require("lazy").setup({
   -- auto-pairing of chars
   {
     "windwp/nvim-autopairs",
-    opts = {}
+    opts = specs.default
   },
 
   -- file explorer
@@ -27,136 +29,70 @@ require("lazy").setup({
       "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim"
     },
-    opts = {
-      close_if_last_window = true,
-      filesystem = {
-        filtered_items = {
-          hide_dotfiles = false,
-          hide_gitignored = false,
-          hide_hidden = false,
-          never_show = { ".git" }
-        }
-      },
-      window = { width = 30 }
-    }
+    opts = specs.neotree
+  },
+
+  -- fzf no telescope
+  {
+    "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = specs.default
   },
 
   -- git decorators
   {
     "lewis6991/gitsigns.nvim",
-    opts = {}
+    opts = specs.default
   },
 
   -- harpoon-like navigation
   {
     "cbochs/grapple.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-      style = "basename",
-      win_opts = {
-        width = 30,
-        height = 10
-      }
-    }
+    opts = specs.grapple
   },
 
-  -- key-bindings menu
-  {
-    "folke/which-key.nvim",
-    opts = {}
-  },
-
-  -- lsp and cmp
+  -- lsp + cmp + snippy
   {
     "neovim/nvim-lspconfig",
+    dependencies = { "hrsh7th/cmp-nvim-lsp" },
+    -- due to external lib use, it is not in specs
+    config = function()
+      local lspconfig = require("lspconfig")
+      local cmp_lsp = require("cmp_nvim_lsp")
+
+      for name, lsp in pairs(specs.lsps) do
+        lsp.capabilities = cmp_lsp.default_capabilities()
+        lspconfig[name].setup(lsp)
+      end
+    end
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
     dependencies = {
       "dcampos/nvim-snippy",
-      "hrsh7th/nvim-cmp",
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
       "onsails/lspkind.nvim"
     },
+    -- due to external lib use, it is not in specs
     config = function()
-      local function get_conf(cmd, settings)
-        local new_conf = {}
-
-        new_conf.capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-        if cmd then
-          new_conf.cmd = { "micromamba", "run", "-n", "lsp" }
-          for _, arg in ipairs(cmd) do
-            table.insert(new_conf.cmd, arg)
-          end
-        end
-
-        if settings then
-          new_conf.settings = settings
-        end
-
-        return new_conf
-      end
-
-      local python_path = vim.fn.exepath("python")
-
-      -- if python3 exists, use that instead
-      if vim.fn.executable("python3") == 1 then
-        python_path = vim.fn.exepath("python3")
-      end
-
-      local confs = {
-        bashls        = get_conf({ "bash-language-server", "start" }),
-        clangd        = get_conf(),
-        cssls         = get_conf({ "vscode-css-language-server", "--stdio" }),
-        html          = get_conf({ "vscode-html-language-server", "--stdio" }),
-        jsonls        = get_conf({ "vscode-json-language-server", "--stdio" }),
-        lua_ls        = get_conf(),
-        pyright       = get_conf(
-          { "pyright-langserver", "--stdio" },
-          {
-            python = {
-              analysis = {
-                -- strict type checking can be a bit of a pain
-                typeCheckingMode = "off"
-              },
-              -- telling pyright what the current python environment is
-              pythonPath = python_path
-            }
-          }
-        ),
-        ruff_lsp      = get_conf({ "ruff-lsp" }),
-        rust_analyzer = get_conf(),
-        tsserver      = get_conf({ "typescript-language-server", "--stdio" })
-      }
-
-      for name, conf in pairs(confs) do
-        require("lspconfig")[name].setup(conf)
-      end
-
       local cmp = require("cmp")
+      local lspkind = require("lspkind")
+      local snippy = require("snippy")
+
       cmp.setup({
         completion = { completeopt = "menu,menuone,noinsert" },
-        formatting = {
-          format = require("lspkind").cmp_format({
-            mode = "symbol_text",
-            maxwidth = 50,
-            ellipsis_char = "...",
-            menu = {
-              nvim_lsp = "",
-              buffer = ""
-            }
-          })
-        },
+        formatting = { format = lspkind.cmp_format(specs.lspkind) },
         mapping = {
-          ["<C-y>"] = cmp.mapping.confirm({ select = false }),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = "select" }),
-          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = "select" }),
-          ["<C-d>"] = cmp.mapping.scroll_docs(5),
-          ["<C-u>"] = cmp.mapping.scroll_docs(-5)
+          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item()
         },
         snippet = {
           expand = function(args)
-            require("snippy").expand_snippet(args.body)
+            snippy.expand_snippet(args.body)
           end
         },
         sorting = {
@@ -165,19 +101,7 @@ require("lazy").setup({
             cmp.config.compare.exact,
             cmp.config.compare.score,
             cmp.config.compare.recently_used,
-            -- sorts completion items with one or more underscores
-            -- helpful in python especially
-            function(entry1, entry2)
-              local _, entry1_under = entry1.completion_item.label:find("^_+")
-              local _, entry2_under = entry2.completion_item.label:find("^_+")
-              entry1_under = entry1_under or 0
-              entry2_under = entry2_under or 0
-              if entry1_under > entry2_under then
-                return false
-              elseif entry1_under < entry2_under then
-                return true
-              end
-            end,
+            helpers.get_cmp_under_comparator,
             cmp.config.compare.locality,
             cmp.config.compare.kind
           }
@@ -194,103 +118,29 @@ require("lazy").setup({
     end
   },
 
-  -- overall prettier ui
+  {
+    "dcampos/nvim-snippy",
+    opts = specs.snippy
+  },
+
+  -- overall prettier ui (without nvim-notify)
   {
     "folke/noice.nvim",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      {
-        "rcarriga/nvim-notify",
-        opts = {
-          minimum_width = 25,
-          render = "compact",
-          timeout = 500
-        }
-      }
-    },
-    opts = {
-      lsp = {
-        progress = { enabled = false },
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true
-        },
-        hover = {
-          opts = {
-            size = {
-              max_width = 50,
-              max_height = 12
-            }
-          }
-        },
-        signature = {
-          opts = {
-            size = {
-              max_width = 50,
-              max_height = 12
-            }
-          }
-        }
-      },
-      presets = {
-        command_palette = true,
-        lsp_doc_border = true
-      }
-    }
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = specs.noice
   },
 
   -- statusline
   {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-      sections = {
-        lualine_b = { "branch", "diagnostics" },
-        lualine_x = {
-          -- displays register if present
-          function()
-            local reg = vim.fn.reg_recording()
-            if reg == "" then
-              return ""
-            end
-            return "recording @" .. reg
-          end,
-          "filetype"
-        },
-        lualine_y = { "grapple" }
-      }
-    }
-  },
-
-  -- telescope
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons"
-    },
-    branch = "0.1.x",
-    opts = {
-      pickers = {
-        find_files = {
-          find_command = { "fd", "--hidden", "--type", "f" }
-        },
-        live_grep = {
-          additional_args = { "--hidden", "--glob=!.git" }
-        }
-      }
-    }
+    opts = specs.lualine
   },
 
   -- terminal
   {
     "akinsho/toggleterm.nvim",
-    opts = {
-      direction = "float",
-      open_mapping = [[<C-\>]],
-      persist_mode = true
-    }
+    opts = specs.toggleterm
   },
 
   -- theme
@@ -308,28 +158,7 @@ require("lazy").setup({
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          -- the following should always be installed
-          "c",
-          "lua",
-          "vim",
-          "vimdoc",
-          "query",
-
-          -- the following is required by noice.nvim
-          "regex",
-          "bash",
-          "markdown",
-          "markdown_inline"
-        },
-        auto_install = true,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = true
-        },
-        indent = { enable = true }
-      })
+      require("nvim-treesitter.configs").setup(specs.treesitter)
     end
   }
 })
