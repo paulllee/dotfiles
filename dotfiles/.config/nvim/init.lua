56a78ce523a77d713242d60d3a48bc4f15a06c04
Paul Lee <paulllee.md@gmail.com>
2024-05-25 00:49:03 -0400

    remove noice due to bloat

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -38,269 +38,78 @@ vim.o.undofile = true
 
 -- [[ PLUGINS ]]
 
--- install lazy manager
-local lazy = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazy) then
+-- install lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazy
+    "--branch=stable", -- latest stable release
+    lazypath,
   })
 end
-vim.opt.rtp:prepend(lazy)
-
-local ui_width = 40
-local ui_height = 12
+vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  -- auto-pairing of chars
   {
     "windwp/nvim-autopairs",
     opts = {}
   },
 
-  -- cmp
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "dcampos/nvim-snippy",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "onsails/lspkind.nvim"
-    },
-    config = function()
-      local cmp = require("cmp")
-      local lspkind = require("lspkind")
-      local snippy = require("snippy")
-
-      cmp.setup({
-        completion = { completeopt = "menu,menuone,noinsert" },
-        formatting = {
-          format = lspkind.cmp_format({
-            mode = "symbol_text",
-            maxwidth = ui_width,
-            ellipsis_char = "...",
-            menu = {
-              nvim_lsp = "",
-              buffer = ""
-            }
-          })
-        },
-        mapping = {
-          ["<C-y>"] = cmp.mapping.confirm(),
-          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = "select" }),
-          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = "select" })
-        },
-        snippet = {
-          expand = function(args)
-            snippy.expand_snippet(args.body)
-          end
-        },
-        sorting = {
-          comparators = {
-            cmp.config.compare.offset,
-            cmp.config.compare.exact,
-            cmp.config.compare.score,
-            cmp.config.compare.recently_used,
-            function(a, b)
-              local _, us1 = a.completion_item.label:find("^_+")
-              local _, us2 = b.completion_item.label:find("^_+")
-              us1 = us1 or 0
-              us2 = us2 or 0
-              if us1 > us2 then
-                return false
-              elseif us1 < us2 then
-                return true
-              end
-            end,
-            cmp.config.compare.locality,
-            cmp.config.compare.kind
-          }
-        },
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "buffer" }
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered()
-        }
-      })
-    end
-  },
-
-  -- file explorer
-  {
-    "stevearc/oil.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-      view_options = {
-        show_hidden = true,
-        is_always_hidden = function(name, _)
-          if name == ".git" then
-            return true
-          end
-          return false
-        end
-      },
-      float = {
-        max_width = ui_width,
-        max_height = ui_height
-      }
-    }
-  },
-
-  -- git decorators
   {
     "lewis6991/gitsigns.nvim",
     opts = {}
   },
 
-  -- harpoon-like navigation
+  {
+    "stevearc/dressing.nvim",
+    opts = {},
+  },
+
+  {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      view_options = { show_hidden = true }
+    }
+  },
+
   {
     "cbochs/grapple.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-      style = "basename",
-      win_opts = {
-        width = ui_width,
-        height = ui_height
-      }
-    }
-
+    opts = {}
   },
 
-  -- lsp
   {
-    "neovim/nvim-lspconfig",
-    dependencies = { "hrsh7th/cmp-nvim-lsp" },
-    config = function()
-      local lspconfig = require("lspconfig")
-      local cmp_lsp = require("cmp_nvim_lsp")
-
-      local configs = {
-        clangd = {},
-        jsonls = {},
-        lua_ls = {},
-        pyright = {
-          settings = {
-            python = {
-              analysis = {
-                typeCheckingMode = "off"
-              },
-              pythonPath = function()
-                if vim.fn.executable("python3") == 1 then
-                  return vim.fn.exepath("python3")
-                end
-                return vim.fn.exepath("python")
-              end
-            }
-          }
-        },
-        ruff_lsp = {
-          on_attach = function(client, _)
-            client.server_capabilities.hoverProvider = false
-          end
-        }
-      }
-
-      for name, config in pairs(configs) do
-        config.capabilities = cmp_lsp.default_capabilities()
-        lspconfig[name].setup(config)
-      end
-    end
-  },
-
-  -- notify customization
-  {
-    "rcarriga/nvim-notify",
+    "j-hui/fidget.nvim",
     opts = {
-      render = "compact",
-      stages = "static",
-      timeout = 2500
+      notification = { override_vim_notify = true }
     }
   },
 
-  -- prettier ui
   {
-    "folke/noice.nvim",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify"
-    },
+    "akinsho/toggleterm.nvim",
     opts = {
-      lsp = {
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true
-        },
-        hover = {
-          opts = {
-            size = {
-              max_width = ui_width,
-              max_height = ui_height
-            }
-          }
-        },
-        signature = {
-          opts = {
-            size = {
-              max_width = ui_width,
-              max_height = ui_height
-            }
-          }
-        }
-      },
-      presets = {
-        command_palette = true,
-        lsp_doc_border = true
-      }
-    }
-
-
-  },
-
-  -- snippy
-  {
-    "dcampos/nvim-snippy",
-    opts = {
-      mappings = {
-        is = {
-          ["<Tab>"] = "next",
-          ["<S-Tab>"] = "previous"
-        }
-      }
+      direction = "float",
+      open_mapping = [[<C-\>]],
+      persist_mode = true
     }
   },
 
-  -- statusline
   {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
       sections = {
         lualine_b = { "branch", "diagnostics" },
-        lualine_x = {
-          function()
-            local reg = vim.fn.reg_recording()
-            if reg == "" then
-              return ""
-            end
-            return "recording @" .. reg
-          end
-          ,
-          "filetype"
-        },
+        lualine_x = { "filetype" },
         lualine_y = { "grapple" }
       }
     }
   },
 
-  -- telescope
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
@@ -311,23 +120,12 @@ require("lazy").setup({
           find_command = { "fd", "--hidden", "--type", "f" }
         },
         live_grep = {
-          additional_args = { "--hidden", "--glob=!.git" }
+          additional_args = { "--hidden", "--glob=!{.git}" }
         }
       }
     }
   },
 
-  -- terminal
-  {
-    "akinsho/toggleterm.nvim",
-    opts = {
-      direction = "float",
-      open_mapping = [[<C-\>]],
-      persist_mode = true
-    }
-  },
-
-  -- theme
   {
     "catppuccin/nvim",
     name = "catppuccin",
@@ -337,7 +135,6 @@ require("lazy").setup({
     end
   },
 
-  -- treesitter
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
@@ -365,14 +162,124 @@ require("lazy").setup({
         indent = { enable = true }
       })
     end
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "hrsh7th/nvim-cmp",
+      "dcampos/nvim-snippy",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "onsails/lspkind.nvim"
+    },
+    config = function()
+      -- solves issue between windows vs mac
+      -- where windows doesn't recognize `python3`
+      local function get_python_path()
+        if vim.fn.executable("python3") == 1 then
+          return vim.fn.exepath("python3")
+        end
+        return vim.fn.exepath("python")
+      end
+
+      -- disables hover to prioritize pyright
+      local function on_attach_ruff(c, _)
+        c.server_capabilities.hoverProvider = false
+      end
+
+      local confs = {
+        clangd = {},
+        jsonls = {},
+        lua_ls = {},
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                typeCheckingMode = "off"
+              },
+              pythonPath = get_python_path()
+            }
+          }
+        },
+        ruff_lsp = { on_attach = on_attach_ruff }
+      }
+
+      for lsp, conf in pairs(confs) do
+        -- adding cmp capabilities to all lsps
+        conf.capabilities = require("cmp_nvim_lsp").default_capabilities()
+        require("lspconfig")[lsp].setup(conf)
+      end
+
+      local cmp = require("cmp")
+
+      cmp.setup({
+        completion = { completeopt = "menu,menuone,noinsert" },
+        formatting = {
+          format = require("lspkind").cmp_format({
+            mode = "symbol_text",
+            maxwidth = 50,
+            ellipsis_char = "...",
+            menu = {
+              nvim_lsp = "",
+              buffer = ""
+            }
+          })
+        },
+        mapping = {
+          ["<C-y>"] = cmp.mapping.confirm(),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = "select" }),
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = "select" })
+        },
+        snippet = {
+          expand = function(args)
+            require("snippy").expand_snippet(args.body)
+          end
+        },
+        sorting = {
+          comparators = {
+            cmp.config.compare.offset,
+            cmp.config.compare.exact,
+            cmp.config.compare.score,
+            cmp.config.compare.recently_used,
+            -- sort completion items with two underscores in a row
+            function(a, b)
+              local _, us1 = a.completion_item.label:find("^_+")
+              local _, us2 = b.completion_item.label:find("^_+")
+              us1 = us1 or 0
+              us2 = us2 or 0
+              if us1 > us2 then
+                return false
+              elseif us1 < us2 then
+                return true
+              end
+            end,
+            cmp.config.compare.locality,
+            cmp.config.compare.kind
+          }
+        },
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "buffer" }
+        }
+      })
+
+      require("snippy").setup({
+        mappings = {
+          is = {
+            ["<Tab>"] = "next",
+            ["<S-Tab>"] = "previous"
+          }
+        }
+      })
+    end
   }
 })
 
--- [[ KEYBINDS ]]
+-- [[ MAPPINGS ]]
 
--- vim.keymap.set wrapper with sane defaults
 local function map(mode, lhs, rhs, desc, buffer)
-  vim.keymap.set(mode, lhs, rhs, {
+  vim.keymap.set(mode, lhs, "<CMD>" .. rhs .. "<CR>", {
     desc = desc,
     buffer = buffer,
     noremap = true,
@@ -380,37 +287,24 @@ local function map(mode, lhs, rhs, desc, buffer)
   })
 end
 
--- all rhs default to <CMD>rhs<CR> format
-local function cmap(mode, lhs, rhs, desc, buffer)
-  map(mode, lhs, "<CMD>" .. rhs .. "<CR>", desc, buffer)
-end
+map("n", "<Leader>e", "Oil --float")
 
--- to go between windows more conveniently
-map("n", "<C-w><C-h>", "<C-w>h")
-map("n", "<C-w><C-j>", "<C-w>j")
-map("n", "<C-w><C-k>", "<C-w>k")
-map("n", "<C-w><C-l>", "<C-w>l")
+map("n", "<Leader>f", "Telescope find_files")
+map("n", "<Leader>g", "Telescope live_grep")
 
-cmap("n", "<Leader>e", "Oil --float")
-
-cmap("n", "<Leader>f", "Telescope find_files")
-cmap("n", "<Leader>g", "Telescope live_grep")
-
-cmap("n", "<C-s>", "Grapple toggle")
-cmap("n", ",", "Grapple toggle_tags")
+map("n", ",", "Grapple toggle_tags")
+map("n", "<C-s>", "Grapple toggle")
 
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(args)
-    -- wraps cmap for lsp keybinds
     local function lmap(mode, lhs, rhs, desc)
-      cmap(mode, lhs, rhs, desc, args.buf)
+      map(mode, lhs, rhs, desc, args.buf)
     end
+    lmap("n", "gd", "Telescope lsp_definitions")
+    lmap("n", "gr", "Telescope lsp_references")
 
     lmap("n", "gh", "lua vim.lsp.buf.hover()")
     lmap("n", "gs", "lua vim.lsp.buf.signature_help()")
-
-    lmap("n", "gd", "Telescope lsp_definitions")
-    lmap("n", "gr", "Telescope lsp_references")
 
     lmap("n", "<Leader>R", "lua vim.lsp.buf.rename()")
     lmap("n", "<Leader>F", "lua vim.lsp.buf.format()")
