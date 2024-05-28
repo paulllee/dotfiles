13972cec613a71953c41cf3b7549f988d0a69bb1
Paul Lee <paulllee.md@gmail.com>
2024-05-28 19:20:04 -0400

    merge lsp and ds together with -e flag

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ set -e
 
 rm -rf "$HOME/.dotfiles" && \
   git clone --depth=1 https://github.com/paulllee/dotfiles.git "$HOME/.dotfiles" && \
-  /usr/bin/env bash "$HOME/.dotfiles/dotfiles/.local/bin/ds" -dgmp
+  /usr/bin/env bash "$HOME/.dotfiles/dotfiles/.local/bin/ds" -degmp
 
 printf "Changing default shell to fish will prompt for sudo access.\n"
 echo "/opt/homebrew/bin/fish" | sudo tee -a "/etc/shells" && \
