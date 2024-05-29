c72099bfc6ed2bdaf889cfac152b3cad7a0e78df
Paul Lee <paulllee.md@gmail.com>
2024-05-29 15:01:36 -0400

    revert "remove default opts for fzf"
    this reverts commit 2e08f1be44c4ae0cb3f1a054cc9242e649343600
    
    i want this back actually...

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -18,6 +18,11 @@ end
 set -gx EDITOR nvim
 set -gx VISUAL "$EDITOR"
 
+set -gx FZF_DEFAULT_OPTS "--border=rounded \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+
 # PACKAGES
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
