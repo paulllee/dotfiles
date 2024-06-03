268cde8688ff6bf943d2cd959badf45374dbfe99
Paul Lee <paulllee.md@gmail.com>
2024-06-03 13:27:26 -0400

    renable indents via treesitter
    python was scuffed with indentation without it

--- dotfiles/.config/nvim/lua/core/plugins.lua
+++ dotfiles/.config/nvim/lua/core/plugins.lua
@@ -59,7 +59,8 @@ require("lazy").setup({
     config = function()
       require("nvim-treesitter.configs").setup({
         auto_install = true,
-        highlight = { enable = true }
+        highlight = { enable = true },
+        indent = { enable = true }
       })
     end
   },
