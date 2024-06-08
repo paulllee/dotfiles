1cec25ba9bd288893075b0b6c6a4416b6a3ca404
Paul Lee <paulllee.md@gmail.com>
2024-06-08 16:56:25 -0400

    feat(nvim): scrolloff=10 and organize

--- dotfiles/.config/nvim/lua/core/options.lua
+++ dotfiles/.config/nvim/lua/core/options.lua
@@ -1,26 +1,23 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.g.loaded_python3_provider = 0
-vim.g.loaded_perl_provider = 0
-vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
-
-vim.wo.number = true
-vim.wo.relativenumber = true
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
 
 vim.o.expandtab = true
 vim.o.shiftwidth = 2
 vim.o.softtabstop = 2
 vim.o.tabstop = 2
 
-vim.o.wrap = false
-
-vim.o.smartcase = true
-
 vim.o.clipboard = "unnamedplus"
-
-vim.o.undofile = true
-
 vim.o.hlsearch = false
 vim.o.incsearch = true
+vim.o.scrolloff = 10
+vim.o.smartcase = true
+vim.o.undofile = true
+vim.o.wrap = false
+
+vim.wo.number = true
+vim.wo.relativenumber = true
