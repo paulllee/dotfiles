8581995a91e01e380625e14861632666bf3f0730
Paul Lee <paulllee.md@gmail.com>
2024-06-11 12:22:03 -0400

    chore(nvim): cleanup autocmd

--- dotfiles/.config/nvim/lua/core/extras.lua
+++ dotfiles/.config/nvim/lua/core/extras.lua
@@ -1,8 +1,7 @@
 vim.api.nvim_create_autocmd("BufWritePre", {
   callback = function()
     local save = vim.fn.winsaveview()
-    local status, _ = pcall(function() vim.cmd([[%s/\s\+$//g]]) end)
-    if status then vim.notify("Removed trailing whitespace[s]") end
+    vim.cmd([[%s/\s\+$//e]])
     vim.fn.winrestview(save)
   end
 })
