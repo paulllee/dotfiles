4fda73bb49e67c5bb76d31ab1996999812cada09
Paul Lee <paulllee.md@gmail.com>
2024-06-12 02:33:41 -0400

    feat(fish): reimplement qcd
    q(uick) c(hange) d(irectory)

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -3,14 +3,14 @@ fish_add_path "$HOME/.local/bin"
 alias lg="lazygit"
 
 function qcd
-  cd "$(ff $argv)"
+  cd "$(fd --hidden --exclude '.git' --type d . | fzf)"
   clear
 end
 
 set -gx EDITOR nvim
 set -gx VISUAL "$EDITOR"
 
-set -gx FZF_DEFAULT_OPTS "--border=rounded \
+set -gx FZF_DEFAULT_OPTS "\
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
