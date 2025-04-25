4bcfd1a8f50bbd06f0e49d1c677e6fc3e76856ac
Paul Lee <paulllee.md@gmail.com>
2025-04-24 23:37:24 -0400

    feat(wezterm): windows specific font size

--- dotfiles/.config/wezterm/wezterm.lua
+++ dotfiles/.config/wezterm/wezterm.lua
@@ -1,15 +1,14 @@
-local wt = require("wezterm")
-local config = wt.config_builder()
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Latte"
-
 config.enable_tab_bar = false
-
-config.font = wt.font("JetBrains Mono")
+config.font = wezterm.font("JetBrains Mono")
 config.font_size = 14.0
 
-if wt.target_triple:find("windows") ~= nil then
+if wezterm.target_triple:find("windows") ~= nil then
   config.default_prog = { "pwsh.exe" }
+  config.font_size = 11.0
 end
 
 return config
