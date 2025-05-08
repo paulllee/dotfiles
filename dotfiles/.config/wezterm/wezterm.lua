c6d36ec78257397d8ee89c3682d6ea8bc592b523
Paul Lee <paulllee.md@gmail.com>
2025-05-08 19:52:18 -0400

    feat(theme): dark mode user again!

--- dotfiles/.config/wezterm/wezterm.lua
+++ dotfiles/.config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = "Catppuccin Latte"
+config.color_scheme = "Catppuccin Mocha"
 config.enable_tab_bar = false
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 14.0
--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -28,11 +28,11 @@ function in {
 $Env:EDITOR = "nvim"
 
 $Env:FZF_DEFAULT_OPTS=@"
---color=bg+:#ccd0da,bg:#eff1f5,spinner:#dc8a78,hl:#d20f39
---color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78
---color=marker:#7287fd,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39
---color=selected-bg:#bcc0cc
---color=border:#ccd0da,label:#4c4f69
+--color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8
+--color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC
+--color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8
+--color=selected-bg:#45475A
+--color=border:#313244,label:#CDD6F4
 "@
 
 Invoke-Expression (&starship init powershell)
