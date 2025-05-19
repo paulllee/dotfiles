8ed074c32c14438f6b56386510bb9fa4d6c95a28
Paul Lee <paulllee.md@gmail.com>
2025-05-19 02:05:38 +0000

    fix(windows): scoop sync proper

--- windows/Buckets.ps1
+++ windows/Buckets.ps1
@@ -1,4 +1,21 @@
-$Buckets = @(
+$DesiredBuckets = @(
+    "main",
     "extras",
-    "nerd-fonts",
+    "nerd-fonts"
 )
+
+function Get-AddedBuckets {
+    return scoop bucket list | ForEach-Object { $_.Name }
+}
+
+function Sync-Buckets {
+    $AddedBuckets = Get-AddedBuckets
+
+    $DesiredBuckets `
+        | Where-Object { $_ -notin $AddedBuckets } `
+        | ForEach-Object { scoop bucket add $_ }
+
+    $AddedBuckets `
+        | Where-Object { $_ -notin $DesiredBuckets } `
+        | ForEach-Object { scoop bucket rm $_ }
+}
--- windows/Sync.ps1
+++ windows/Sync.ps1
@@ -1,35 +1,13 @@
 . "$PSScriptRoot\Apps.ps1"
 . "$PSScriptRoot\Buckets.ps1"
 
-# git needed prior to adding buckets
-scoop install git
+# git is needed prior to adding buckets
+$InstalledApps = Get-InstalledApps
+if ("git" -notin $InstalledApps) {
+    scoop install git
+}
 
-# get buckets that is currently added
-$Added = scoop bucket list | ForEach-Object { ($_ -split ' ')[0] }
+Sync-Buckets
+Sync-Apps
 
-# uninstall buckets that aren't in the list
-$Added `
-    | Where-Object { $_ -notin $Buckets } `
-    | ForEach-Object { scoop bucket rm $_ }
-
-# install missing buckets
-$Buckets `
-    | Where-Object { $_ -notin $Added } `
-    | ForEach-Object { scoop bucket add $_ }
-
-# get packages that are currently installed
-$Installed = scoop list | ForEach-Object { ($_ -split ' ')[0] }
-
-# uninstall apps that aren't in the list
-$Installed `
-    | Where-Object { $_ -notin $Apps } `
-    | ForEach-Object { scoop uninstall $_ }
-
-# install missing apps
-$Apps `
-    | Where-Object { $_ -notin $Installed } `
-    | ForEach-Object { scoop install $_ }
-
-# Update installed apps
-$Installed `
-    | ForEach-Object { scoop update $_ }
+scoop cleanup -a
